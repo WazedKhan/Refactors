3e21e889162056e04121950eaedfc3e927e50ba1
WazedKhan <wazedkhan119399@gmail.com>
2024-08-03 22:12:38 +0600

    function based refactor complete

--- Gilded-Rose/gilded_rose.py
+++ Gilded-Rose/gilded_rose.py
@@ -18,26 +18,30 @@ def increase_item_quality(item: Item, amount: int = 1, max_quality: int = 50) ->
 
 def update_quality_single(item: Item) -> None:
 
-    if item.name != SULFURAS:
+    if item.name == SULFURAS:
+        pass
+    else:
         item.sell_in = item.sell_in - 1
 
-    if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-        if item.name != SULFURAS:
-            decrease_item_quality(item)
-    else:
+    if item.name == AGED_BRIE:
         increase_item_quality(item)
-        if item.name == BACKSTAGE_PASSES:
-            if item.sell_in < 10:
-                increase_item_quality(item)
-            if item.sell_in < 5:
-                increase_item_quality(item)
 
-    if item.sell_in < 0:
-        if item.name != "Aged Brie":
-            if item.name != BACKSTAGE_PASSES:
-                if item.name != SULFURAS:
-                    decrease_item_quality(item)
-            else:
-                item.quality = 0
-        else:
+        if item.sell_in < 0:
             increase_item_quality(item)
+
+    elif item.name == BACKSTAGE_PASSES:
+        increase_item_quality(item)
+        if item.sell_in < 10:
+            increase_item_quality(item)
+        if item.sell_in < 5:
+            increase_item_quality(item)
+
+        if item.sell_in < 0:
+            item.quality = 0
+
+    elif item.name == SULFURAS:
+        pass
+    else:
+        decrease_item_quality(item)
+        if item.sell_in < 0:
+            decrease_item_quality(item)
